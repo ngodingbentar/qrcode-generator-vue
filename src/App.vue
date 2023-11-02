e74456d684465a17a41299d41fe07628a9f1b100
ngodingbentar <dewaqintoro@gmail.com>
2023-11-02 23:25:27 +0700

    updated: color

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,17 @@
 <template>
-  <div class="qrcode">
-    <h3>QRCode Generator</h3>
-    <div class="container">
-      <canvas class="canvas"></canvas>
+  <div>
+    <div class="nav">
+      <h3>Ngodingbentar</h3>
     </div>
-    <input placeholder="Enter your website or text here" id="qrInput" />
-    <button @click="generateQR()">generate</button>
-    <p v-if="isError">{{isError}}</p>
+    <div class="qrcode">
+      <h3>QRCode Generator</h3>
+      <div class="container">
+        <canvas class="canvas"></canvas>
+      </div>
+      <input placeholder="Enter your website or text here" id="qrInput" />
+      <button @click="generateQR()" class="btn">Generate</button>
+      <p v-if="isError">{{isError}}</p>
+    </div>
   </div>
 </template>
 
@@ -32,25 +37,48 @@ const generateQR = () => {
 </script>
 
 <style>
-    .qrcode {
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      justify-content: center;
-    }
+  body {
+    margin: 0px;
+  }
+  .nav {
+    background: #0984e3;
+    padding: 1px;
+    color: white;
+  }
 
-    .qrcode .container {
-      width: 200px;
-      height: 200px;
-      border: 2px solid black;
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      justify-content: center;
-    }
+  .nav h3 {
+    margin-left: 20px;
+  }
+  .qrcode {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    font-weight: 700;
+  }
 
-    input {
-      width: 250px;
-      margin: 20px;
-    }
+  .qrcode .container {
+    width: 200px;
+    height: 200px;
+    border: 2px solid black;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
+
+  input {
+    width: 250px;
+    margin: 20px;
+    padding: 10px;
+    border-radius: 10px;
+  }
+
+  .btn {
+    padding: 10px;
+    background-color: #0984e3;
+    color: white;
+    border: none;
+    border-radius: 10px;
+  }
   </style>
